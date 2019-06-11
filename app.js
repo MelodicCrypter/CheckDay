805fa2308bfd83faf31e40af3f6e0ca9044015c4
Hugh Caluscusin <hugh@melodiccrypter.com>
2019-06-11 22:44:34 +0800

    modified app.js, added specific messages

--- app.js
+++ app.js
@@ -37,21 +37,21 @@ geocode.geocodeAddress(argv.a, (errorMessage, geoResults) => {
                 } else if (weatherResults.icon === "snow") {
                     console.log(`Weather in ${address} is ${emoji.emojify(':snowflake:')}snowy => ${temperature}`);
                 } else if (weatherResults.icon === "sleet") {
-                    console.log(`Weather in ${address} is ${emoji.emojify(':snowman:')}sleet => ${temperature}`);
+                    console.log(`Weather in ${address} is ${emoji.emojify(':snowman:')}sleety => ${temperature}`);
                 } else if (weatherResults.icon === "wind") {
-                    console.log(`Weather in ${address} is ${emoji.emojify(':rain_cloud:')}rainy => ${temperature}`);
+                    console.log(`Weather in ${address} is ${emoji.emojify(':wind_blowing_face:')}windy => ${temperature}`);
                 } else if (weatherResults.icon === "fog") {
-                    console.log(`Weather in ${address} is ${emoji.emojify(':rain_cloud:')}rainy => ${temperature}`);
+                    console.log(`Weather in ${address} is ${emoji.emojify(':fog:')}foggy => ${temperature}`);
                 } else if (weatherResults.icon === "cloudy") {
-                    console.log(`Weather in ${address} is ${emoji.emojify(':rain_cloud:')}rainy => ${temperature}`);
+                    console.log(`Weather in ${address} is ${emoji.emojify(':cloud:')}cloudy => ${temperature}`);
                 } else if (weatherResults.icon === "clear-day") {
-                    console.log(`Weather in ${address} is ${emoji.emojify(':rain_cloud:')}rainy => ${temperature}`);
+                    console.log(`Weather in ${address} is ${emoji.emojify(':sun_small_cloud:')}clear and sunny => ${temperature}`);
                 } else if (weatherResults.icon === "clear-night") {
-                    console.log(`Weather in ${address} is ${emoji.emojify(':rain_cloud:')}rainy => ${temperature}`);
+                    console.log(`Tonight, the weather in ${address} is ${emoji.emojify(':full_moon_with_face:')}clear and moony => ${temperature}`);
                 } else if (weatherResults.icon === "partly-cloudy-day") {
-                    console.log(`Weather in ${address} is ${emoji.emojify(':rain_cloud:')}rainy => ${temperature}`);
+                    console.log(`Weather in ${address} is ${emoji.emojify(':sun_behind_cloud:')}partly cloudy => ${temperature}`);
                 } else if (weatherResults.icon === "partly-cloudy-night") {
-                    console.log(`Weather in ${address} is ${emoji.emojify(':rain_cloud:')}rainy => ${temperature}`);
+                    console.log(`Tonight, the weather in ${address} is ${emoji.emojify(':cloud:')}partly cloudy => ${temperature}`);
                 } else {
                     console.log('...');
                 }
